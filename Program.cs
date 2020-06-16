813519b6f0f937117473d0d4362cba73b03562f7
박창용 <tempus157@gmail.com>
2020-06-16 19:02:15 +0900

    가장 최근 디렉토리를 리턴하는 메서드 구현

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using System.Net;
+using System.Linq;
+using System.IO;
+using System;
 
 namespace Saveswapper
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class Saveswapper
+    {
+        private static readonly string SavePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Packages\Microsoft.SunriseBaseGame_8wekyb3d8bbwe\SystemAppData\wgs";
+
+        public static void Swap(string sourceSaveName, string destSaveName)
+        {
+            var sourceSaveDirectories = new DirectoryInfo($@"{SavePath}\{sourceSaveName}").GetDirectories();
+            
+        }
+
+        private static DirectoryInfo LatestDirectory(DirectoryInfo[] directories)
+        {
+            return directories.Aggregate((current, next) => current.LastWriteTime > next.LastWriteTime ? current : next);
         }
     }
 }
